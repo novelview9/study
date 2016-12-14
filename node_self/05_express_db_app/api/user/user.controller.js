40c1172c9c995ca9116a8bc3d1e4e9d40cd3cf0d
Luke <novelview9@example.com>
2016-12-15 04:58:22 +0900

    init testcode

---
+++ node_self/05_express_db_app/api/user/user.controller.js
@@ -0,0 +1,15 @@
+let users = [
+  {
+    id: 1,
+    name: 'alice'
+  },
+  {
+    id: 2,
+    name: 'bek'
+  },
+  {
+    id: 3,
+    name: 'chris'
+  }
+];
+exports.index = (req, res) => { return res.json(users) };
--- node_self/05_express_db_app/app.js
+++ node_self/05_express_db_app/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.use("/users", require("./api/user/index"));
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
 
@@ -8,3 +9,5 @@ app.listen(3000, () => {
             console.log('Databases sync');
         });
 });
+
+module.exports = app;
